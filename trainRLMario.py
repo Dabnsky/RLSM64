3e52f707af421c2082f19ab20846b1f70da60148
David <davidhabinsky@gmail.com>
2026-02-17 08:22:09 -0700

    first commit

---
+++ trainRLMario.py
@@ -0,0 +1,17 @@
+from stable_baselines3 import PPO
+from LearnMario import MarioEnv
+
+env = MarioEnv()
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=50000)
+
+model.save("mario_rl_model")
+
+model = PPO.load("mario_rl_model")
+
+obs = env.reset()
+while True:
+    action, _ = model.predict(obs)
+    obs, reward, done, _ = env.step(action)
+    env.render()  # Show the frames for debugging
+    
